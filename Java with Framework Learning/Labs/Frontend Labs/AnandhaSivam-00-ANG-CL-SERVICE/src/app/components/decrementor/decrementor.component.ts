c8b68ef9fdba1c1476a23e3614f4a83273479619
AnandhaSivam-00 <anandsugan218@gmail.com>
2025-10-25 21:20:29 +0530

    MAJOR: Added the Frontend learning folders and exercises

---
+++ Java with Framework Learning/Labs/Frontend Labs/AnandhaSivam-00-ANG-CL-SERVICE/src/app/components/decrementor/decrementor.component.ts
@@ -0,0 +1,31 @@
+import {Component, EventEmitter, OnInit, Output} from '@angular/core';
+import {CountService} from "../../services/count.service";
+
+@Component({
+  selector: 'app-decrementor',
+  templateUrl: './decrementor.component.html',
+  styleUrls: ['./decrementor.component.css']
+})
+export class DecrementorComponent implements OnInit {
+  @Output()
+  decrementEvent : EventEmitter<void> = new EventEmitter<void>();
+
+  /**
+   * TODO: the CountService should be injected into this component as is done in the incrementor component.
+   */
+  constructor(private countService : CountService) { }
+
+  ngOnInit(): void {
+
+  }
+
+  /**
+   * TODO: after writing a decrementNum() method in the CountService AND the CountService service has been injected
+   * into this component, the service's decrementNum method should be called prior to emitting the decrementEvent,
+   * just as has been done in the incrementor component.
+   */
+  emitDecrementEvent() {
+    this.countService.decrementNum();
+    this.decrementEvent.emit();
+  }
+}
